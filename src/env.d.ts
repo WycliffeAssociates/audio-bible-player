16bbf6f7f6316ee4fca06c86f13d50521d4252e8
Will Kelly <willkelly9617@gmail.com>
2024-04-02 11:08:35 -0500

    remove a ts ignore

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    runtime: {
+      env: Record<any, any>;
+    };
+  }
+}
