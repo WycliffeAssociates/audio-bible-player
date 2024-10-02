79b1abd88ca40687acc4bd1865bd61a234137c44
Will Kelly <willkelly9617@gmail.com>
2024-10-02 15:10:31 -0400

    add icons to manifest

--- manifest.ts
+++ manifest.ts
@@ -13,5 +13,41 @@ export const manifest: Partial<ManifestOptions> = {
   // display: "minimal-ui",
   display: "minimal-ui",
   start_url: "/",
-  icons: [],
+  icons: [
+    {
+      src: "icons/icon-72x72.png",
+      sizes: "72x72",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-96x96.png",
+      sizes: "96x96",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-128x128.png",
+      sizes: "128x128",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-144x144.png",
+      sizes: "144x144",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-152x152.png",
+      sizes: "152x152",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "icons/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
 };
