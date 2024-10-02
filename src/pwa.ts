e1f0d06b417b1592dd199f08275665d978b5882c
Will Kelly <willkelly9617@gmail.com>
2024-10-02 14:11:44 -0400

    init commit

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,12 +1,36 @@
 import {registerSW} from "virtual:pwa-register";
 
+// 5 minute
+const intervalMS = 5 * 60 * 1000;
+// https://vite-pwa-org.netlify.app/guide/periodic-sw-updates.html#periodic-service-worker-updates
+console.log("calling first registerSW");
 registerSW({
-  immediate: true,
-  onRegisteredSW(swScriptUrl) {
-    console.log("SW registered: ", swScriptUrl);
+  onRegisteredSW(swUrl, r) {
+    r &&
+      setInterval(async () => {
+        if (!(!r.installing && navigator)) return;
+
+        if ("connection" in navigator && !navigator.onLine) return;
+        console.log("updating sw", swUrl);
+        const resp = await fetch(swUrl, {
+          cache: "no-store",
+          headers: {
+            cache: "no-store",
+            "cache-control": "no-cache",
+          },
+        });
+
+        if (resp?.status === 200) await r.update();
+      }, intervalMS);
+  },
+  onRegisterError(error) {
+    console.error(error);
+  },
+  onNeedRefresh() {
+    // window.location.reload();
+    console.log("onNeedRefresh");
   },
   onOfflineReady() {
-    console.log("PWA application ready to work offline");
+    console.log("onOfflineReady");
   },
 });
-// \.zip(\?.*)?$
